723e5e470f46ba1a2a2f5e2401eee0e0bf9d735d
Lorena Donoso Heredia <75387550+Lore27d@users.noreply.github.com>
2023-03-22 12:45:23 -0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,62 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import statsmodels
 import pickle
 import shelve
+from datetime import date, time, datetime
 
 
 st.title("Predicción de Temperatura")
 
+
 # Levantamos los modelos
-modelos = shelve.open("TP_INTEGRADOR/modelos_y_data.db")
+modelos = shelve.open("./modelos_y_data.db")
 model_est = modelos["model_est"]
 results_ARIMA = modelos["model_arima"]
 
 
+# Levantamos el df_test con las predicciones de test
+with open('./df_test.pkl', 'rb') as f_df_test:
+        dataset_test = pickle.load(f_df_test)
 
 # Levantamos el df para la funcion
-with open('TP_INTEGRADOR/df.pkl', 'rb') as f_df:
+with open('./df.pkl', 'rb') as f_df:
         df = pickle.load(f_df)
 
+
+#A = st.date_input(
+#    "Fecha de Inicio",
+#    date(1909, 1, 1)
+#    )
+A = date(1909, 1, 1)
+
+B = st.date_input(
+    "Seleccione la fecha hasta la cual desea realizar su predicción",
+    date(2025, 12, 1)
+    )
+
 # Funcion Prediccion
-def prediccion_fecha(fecha_pred):
+def prediccion_fecha():
+    inicio = A.strftime("%Y") + '-' + A.strftime("%m")
     
+    fin = B.strftime("%Y") + '-' + B.strftime("%m")
+
+
+
     df_pred = pd.DataFrame()
     df_pred.set_index = df.index
     df_pred['Year'] = df.index.year
     df_pred['Month'] = df.index.month
-    df_pred
+    #df_pred
 
-    fecha = datetime.strptime(fecha_pred, '%Y/%m')
+    #fecha = datetime.strptime(fecha_pred, '%Y/%m')
 
     años = []
     año = 2022
-    for i in range(0,(fecha.year-año)+1):
+    for i in range(0,(B.year-año)+1):
         años.append(año)
         año += 1
     
@@ -40,7 +64,7 @@ def prediccion_fecha(fecha_pred):
         if(año != años[-1]):
             i = 12
         elif(año == años[-1]):
-            i= fecha.month
+            i= B.month
         for i in range (0,i):
             df_pred.loc[df_pred.shape[0]] = [año,i+1]
 
@@ -58,7 +82,7 @@ def prediccion_fecha(fecha_pred):
         df_pred['Fecha'][i] = datetime.strptime(str(df_pred['Year'][i]) +'-'+ str(df_pred['Month'][i]), "%Y-%m")   
 
     pred_reg = model_est.predict(df_pred[['timeIndex','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']])
-    pred_arima = results_ARIMA.get_prediction(start='1909-01',end=fecha)
+    pred_arima = results_ARIMA.get_prediction(start= inicio,end=fin)
     pred_arima = pred_arima.predicted_mean
 
 
@@ -67,25 +91,44 @@ def prediccion_fecha(fecha_pred):
     prediccion['Fecha'] = df_pred['Fecha']
 
     prediccion['Temp'] = predicciones
-    return prediccion
+    return prediccion.sort_values(by='Fecha', ascending=False).head(120)
+
+
+st.write(prediccion_fecha())
+
+fig = plt.figure(figsize=(20,8))
+
+data = prediccion_fecha()
+data.set_index('Fecha', inplace=True )
+data['Year'] = data.index.year
+data['Month'] = data.index.month
+
+
+st.write("Prediccion anual")
+plot1 = sns.lineplot(x='Year' ,y='Temp', data=data, markers=True, dashes=False)
+plot1 = plot1.figure
+st.pyplot(plot1)
+
+
+#plt.yticks([5, 15, 20, 25])
+st.write("Prediccion mes a mes")
+plot0 = data.plot(kind = "line", y = ['Temp'])
+plot0 = plot0.figure
+st.pyplot(plot0)
 
 
 
-st.write(prediccion_fecha('2025/2'))
-
-
-fig = plt.figure(figsize=(15,15))
-
-st.pyplot(dataset_test.plot(kind = "line", y = ['temp_min', 'model_ARIMA','predict_est']).figure)
 
 
 
-def RMSE(predicted, actual):
-    mse = (predicted - actual) ** 2
-    rmse = np.sqrt(mse.sum() / mse.count())
-    return rmse
 
-st.write("Error de ARIMA")
-st.write(RMSE(dataset_test['model_ARIMA'], dataset_test['temp_min']))
-st.write("OLS")
-st.write(RMSE(dataset_test['predict_est'], dataset_test['temp_min']))
+
+#def RMSE(predicted, actual):
+#    mse = (predicted - actual) ** 2
+#    rmse = np.sqrt(mse.sum() / mse.count())
+#    return rmse
+
+#st.write("Error de ARIMA")
+#st.write(RMSE(dataset_test['model_ARIMA'], dataset_test['temp_min']))
+#st.write("OLS")
+#st.write(RMSE(dataset_test['predict_est'], dataset_test['temp_min']))
